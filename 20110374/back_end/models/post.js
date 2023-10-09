777a1d975fb9b8a0ef0966301bbbf41d67f72ddf
KhanhTran297 <giakhanh2972002@gmail.com>
2023-10-09 18:12:51 +0700

    feat: add all

---
+++ 20110374/back_end/models/post.js
@@ -0,0 +1,23 @@
+const listpost = [
+  {
+    id: 1,
+    content: "Helloworld",
+    author: "Khanh",
+    listcomment: [
+      { id: 1, content: "Hello" },
+      { id: 2, content: "Hi" },
+    ],
+  },
+  {
+    id: 2,
+    content: "Hello",
+    author: "Nam",
+    listcomment: [
+      { id: 1, content: "Hello" },
+      { id: 2, content: "Hi" },
+    ],
+  },
+];
+module.exports = {
+  listpost,
+};
